de1ecd382dccb4315be3efc45899b080969a7cd8
tuxbear <ole@nisau.no>
2016-07-19 00:34:08 +0200

    package description and versions

--- package.js
+++ package.js
@@ -1,10 +1,11 @@
 Package.describe({
   name: 'tuxbear:collection-class',
   version: '1.0.0',
+
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor Collection Class returns ES6 class instances from Meteor Collections. ',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/tuxbear/meteor-collection-class',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
